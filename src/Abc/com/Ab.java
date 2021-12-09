bada908f1258003f82d78fa201e4e04132f70feb
Mohan Suryawanshi <mohansautomobiles@gmail.com>
2021-12-09 23:40:58 +0530

    first commit project added

---
+++ src/Abc/com/Ab.java
@@ -0,0 +1,22 @@
+package Abc.com;
+
+public class Ab {
+
+public static void main(String []args)
+{
+	int a = 70;
+	if(a<100) {
+		
+	System.out.println("THE NUMBER IS less than 100 ");	
+		
+	} 
+	
+	else if(a <=50 && a>200)
+	{
+		System.out.println("The number between 50 to 100 ");
+	  }
+	else {
+		System.out.println("a is less 50");
+	}
+  }
+}
---
+++ src/sampl/Neva.java
@@ -0,0 +1,10 @@
+package sampl;
+
+public class Neva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("WELLCOME TO HOME");
+	}
+
+}
