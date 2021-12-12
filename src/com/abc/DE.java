ccfeb9a727566bd088125764bfa506d11d780522
Mohan Suryawanshi <mohansautomobiles@gmail.com>
2021-12-12 16:32:47 +0530

    Ab

---
+++ src/com/abc/DE.java
@@ -0,0 +1,33 @@
+package com.abc;
+
+public class DE {
+	// Main driver function
+
+    public static void main(String[] args)
+
+    {
+
+        // Declaring the number
+
+        int number = 110102;
+ 
+
+        // Converting the integer input to string data
+
+        String string_number = Integer.toString(number);
+ 
+
+        // Traversing through the string using for loop
+
+        for (int i = 0; i < string_number.length(); i++) {
+ 
+
+            // Printing the characters at each position
+
+            System.out.println(string_number.charAt(i));
+
+        }
+
+    }
+}
+
